fdbe31c1d09e16fd99ef60c487e23a694226f8c7
szeroki <36236750+Seyz123@users.noreply.github.com>
2020-12-11 00:22:04 +0100

    ...

--- ndd-proxy.js
+++ ndd-proxy.js
@@ -14,18 +14,29 @@ let server = http.createServer(function(request, response) {
     let redirect = records[host];
     
     if(redirect){
-        proxy.web(request, response, {
-            target: "http://127.0.0.1:" + redirect,
-            secure: true,
-            ssl: config.ssl
-        }, error => {
-            if(error){
-                console.log("[INFO] Error when redirecting to port " + redirect + " for host " + host + " !");
-                response.end("No records found.");
-            } else {
-                console.log("[INFO] Redirecting to " + host + " effectued.");
-            }
-        });
+        if(config.https) {
+            proxy.web(request, response, {
+                target: "https://127.0.0.1:" + redirect,
+                secure: true,
+                ssl: config.ssl
+            }, error => {
+                if(error){
+                    console.log("[INFO] Error when redirecting to port " + redirect + " for host " + host + " !");
+                    response.end("No records found.");
+                } else {
+                    console.log("[INFO] Redirecting to " + host + " effectued.");
+                }
+            });
+        } else {
+            proxy.web(request, response, { target: "http://127.0.0.1:" + redirect }, error => {
+                if(error){
+                    console.log("[INFO] Error when redirecting to port " + redirect + " for host " + host + " !");
+                    response.end("No records found.");
+                } else {
+                    console.log("[INFO] Redirecting to " + host + " effectued.");
+                }
+            });
+        }
     } else {
         console.log("[INFO] No redirection found for request " + host + " !");
         response.end("No records found.");
